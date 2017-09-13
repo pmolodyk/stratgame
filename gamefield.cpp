f9b52c1c4f16a8770ce21a760339025b1e403587
Petr <pamolodyk@gmail.com>
2017-09-13 22:22:09 +0300

    Rectangle painting now working

--- gamefield.cpp
+++ gamefield.cpp
@@ -3,4 +3,15 @@
 GameField::GameField()
 {
     area = new QRect(0, 0, 1000, 500);
+    show();
+    //paintEvent(new QPaintEvent(*(area)));
+}
+
+void GameField::paintEvent(QPaintEvent *event)
+{
+    QPainter* painter = new QPainter(this);
+    painter->setPen(Qt::black);
+    painter->drawText(*area, Qt::AlignCenter, "Data");
+    *area = event->rect();
+    painter->drawRect(*area);
 }
